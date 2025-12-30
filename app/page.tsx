009fb48d1268bea16c93eb5bcf843d00742d5dc1
Phyo Htet Paing <phyohtetpaing200303@gmail.com>
2025-12-31 03:05:05 +0700

    implement coin overview and trending coins ui and functionality using the fetcher utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-const Page = () => {
-  return <p className="text-3xl text-indigo-500">Coinpulse</p>;
+import { Suspense } from "react";
+import CoinOverview from "../components/home/CoinOverview";
+import TrendingCoin from "../components/home/TrendingCoin";
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback";
+
+const Page = async () => {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoin />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
